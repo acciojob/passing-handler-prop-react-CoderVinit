b412e9c9e99d5059146dfdae0f9cc1c77910b823
VINIT KUMAR PATEL <101181057+CoderVinit@users.noreply.github.com>
2025-02-22 19:00:05 +0530

    final commit

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,16 @@
+import React, { useEffect, useState } from "react";
 
+const Selection = ({ applyColor }) => {
+  const [style, setStyle] = useState({});
+
+  const handleClick = () => {
+    applyColor(setStyle);
+  };
+  return (
+    <div className="fix-box" style={style} onClick={handleClick}>
+      Selection
+    </div>
+  );
+};
+
+export default Selection;
